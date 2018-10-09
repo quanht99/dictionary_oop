cfcf88e5bac1b175ecc24c07d15128fd3dc0fdb3
tienquan1999 <zed.quan99@gmail.com>
2018-10-10 02:50:08 +0700

    change

--- src/GiaoDien/giaoDien.java
+++ src/GiaoDien/giaoDien.java
@@ -1,76 +1,117 @@
 package GiaoDien;
 
-import controller.controller;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.sql.SQLException;
 
-public class giaoDien extends Frame implements ActionListener {
-    private TextField textField;
-    private Label label;
+import static controller.controller.findWord;
+import static controller.controller.suggestionWord;
 
-    public giaoDien()
-    {
-        Button button = new Button("OK");
-        button.setBounds(50,150,60,30);
+public class giaoDien extends JFrame implements ActionListener, KeyListener
+{
+    private JButton jButton1;
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JTextField jTextField1;
+    private JList jList;
+    private JScrollPane jScrollPane;
 
-        label = new Label();
-        label.setBounds(50,100,250,30);
+    public giaoDien()   {
+        jButton1 = new JButton("OK");
+        jLabel1 = new JLabel("Find");
+        jLabel2 = new JLabel();
+        jTextField1 = new JTextField();
+        jLabel3 = new JLabel();
+        jList = new JList();
+        jScrollPane = new JScrollPane(jList);
+        jTextField1.setFont(new Font("Serif", Font.PLAIN, 16));
 
-        textField = new TextField();
-        textField.setBounds(50,50,150,30);
+        jButton1.setBounds(250, 50, 60, 30);
+        jTextField1.setBounds(50,50,200,30);
+        jLabel1.setBounds(10,50,30,30);
+        jLabel2.setBounds(400,150,200,30);
+        jLabel3.setBounds(400,70,200,30);
+        jList.setBounds(50,90,200,400);
+        jScrollPane.setBounds(50,90,230,400);
+        jButton1.addActionListener(this);
+        jTextField1.addKeyListener(this);
 
-        button.addActionListener(this);
+        add(jButton1);
+        add(jLabel1);
+        add(jLabel2);
+        add(jLabel3);
+        add(jTextField1);
+        add(jScrollPane);
 
-        add(button);
-        add(label);
-        add(textField);
-
-        setSize(400,500);
+        setSize(1000,600);
         setLayout(null);
         setVisible(true);
-        thoatChuongTrinh();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-    }
-    //cop
-    public void thoatChuongTrinh()
-    {
-        this.addWindowListener(new WindowAdapter() {
-            public void windowClosed(WindowEvent e) {
-                JOptionPane.showConfirmDialog(null, "Không có gì xảy ra với sự kiện này nếu tự đối tượng bắt sự kiện này?",
-                        null, JOptionPane.YES_NO_OPTION);
-            }
-            public void windowClosing(WindowEvent e) {
-                int hoi = JOptionPane.showConfirmDialog(null, "Bạn có muốn thoát chương trình không?",
-                        null, JOptionPane.YES_NO_OPTION);
-                if (hoi == JOptionPane.YES_OPTION) {
-                    System.exit(0);
-                }
-            }
-        });
-    }
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        try
-        {
-            String text = textField.getText();
-            String detail = controller.findWord(text);
-            label.setText(detail);
+    public void actionPerformed(ActionEvent e) {
+       abc();
+    }
+
+    public void listWord(){
+        String text = jTextField1.getText();
+        String word[] = {""};
+        try {
+            word = suggestionWord(text);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (Exception ex)
-        {
-            System.out.println(ex);
+        for(int i=0; i<word.length; i++){
+            System.out.println(word[i]);
+        }
+        jList.setListData(word);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //nhan
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            abc();
         }
     }
 
-//    public static void main(String args[])
-//    {
-//        new giaoDien();
-//    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+    //nha
+        if(( e.getKeyCode() >= 65 && e.getKeyCode()<=90 )
+                || e.getKeyCode() ==8
+                || ( e.getKeyCode()>=48 && e.getKeyCode() <=57)
+                || e.getKeyCode()==32){
+            listWord();
+        }
+    }
 
+    private void abc(){
+        String text = jTextField1.getText();
+        String s = "";
+        try {
+            s = findWord(text);
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        if(s.equals("")){
+            JOptionPane.showConfirmDialog(this,"Từ bạn nhập không có trong từ điển", "Thông báo", JOptionPane.DEFAULT_OPTION);
+        }
+        else{
+            System.out.println(s);
+            jLabel3.setText(text);
+            jLabel2.setText(s);
+        }
+    }
 }
--- src/controller/controller.java
+++ src/controller/controller.java
@@ -9,17 +9,27 @@ public class controller {
         Connection conn = connectWithDatabase.getConnection();
         assert conn != null;
         Statement stmt = conn.createStatement();
-        String sql = "select detail from word where word = '" + text +"'";
-        ResultSet rs = stmt.executeQuery(sql);
-        String Detail = "";
-        while(rs.next()){
-            Detail = rs.getString("detail");
+        String sql = "select detail from word where word = ?";
+
+        try{
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, text);
+            ResultSet rs = pstm.executeQuery();
+            String Detail = "";
+            while(rs.next()){
+                Detail = rs.getString("detail");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+            return Detail;
         }
-        rs.close();
-        stmt.close();
-        conn.close();
-        return Detail;
+        catch (Exception e){
+            System.out.println(e);
+            return "";
+        }
     }
+
     public static String[] suggestionWord(String text) throws SQLException {
         int dai;
         dai = text.length();
@@ -28,17 +38,24 @@ public class controller {
         assert conn != null;
         Statement stmt = conn.createStatement();
         String sql = "select word from word where substring(word, 1, ?) = ?" + " limit  ?";
-        PreparedStatement pstm = conn.prepareStatement(sql);
-        pstm.setInt(1, dai);
-        pstm.setString(2, text);
-        pstm.setInt(3, 30);
-        ResultSet rs = pstm.executeQuery();
-        int i=0;
-        while(rs.next()){
-            result[i] = rs.getString("word");
-            i++;
+
+        try{
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, dai);
+            pstm.setString(2, text);
+            pstm.setInt(3, 30);
+            ResultSet rs = pstm.executeQuery();
+            int i=0;
+            while(rs.next()){
+                result[i] = rs.getString("word");
+                i++;
+            }
+            return result;
         }
-        return result;
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
     }
 
 }
